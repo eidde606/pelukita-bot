c0aa7dccd79aa633879d712e0acfa7838f7de81c
eidde606 <eidde_606@yahoo.com>
2025-07-02 23:53:04 -0400

    changed port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 
